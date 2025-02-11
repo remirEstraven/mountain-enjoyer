626d3a421429bff992277b83bc2fa0985b038200
lucas <lucas@peanut-button.lan>
2025-02-10 20:21:23 -0600

    initial commit

---
+++ src/main/java/edu/uah/mountain/enjoyer/MountainEnjoyer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.uah.mountain.enjoyer;
+
+/**
+ *
+ * @author lucas
+ */
+public class MountainEnjoyer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
