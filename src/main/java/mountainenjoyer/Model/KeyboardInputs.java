f22e2acd3bcc2aeda193cb41d8833c17534b81fe
Coleen <coleencorvalan@gmail.com>
2025-03-24 10:23:16 -0500

    Created KeyboardInput class

---
+++ src/main/java/mountainenjoyer/Model/KeyboardInputs.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mountainenjoyer.Model;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+    
+    /**
+    * Invoked when a key has been pressed.
+    * 
+    * @param e the event to be processed
+    */
+    @Override
+    public void keyPressed(KeyEvent e){}
+
+    /**
+    * Invoked when a key has been released.
+    *
+    * @param e the event to be processed
+    */
+    @Override
+    public void keyReleased(KeyEvent e){}
+
+    /**
+    * Invoked when a key has been typed.
+    *
+    * @param e the event to be processed
+    */
+    @Override
+    public void keyTyped(KeyEvent e){}
+}
