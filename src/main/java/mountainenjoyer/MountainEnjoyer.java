b106d3691d74c1c4aaa096336bbd633834bc0b1f
Michael Agnew <128421604+Viceyricey@users.noreply.github.com>
2025-04-01 19:10:16 -0500

    Update MountainEnjoyer.java

--- src/main/java/mountainenjoyer/MountainEnjoyer.java
+++ src/main/java/mountainenjoyer/MountainEnjoyer.java
@@ -3,6 +3,7 @@
  */
 
 package mountainenjoyer;
+import mountainenjoyer.Model.Menu;
 
 /**
  *
@@ -12,5 +13,7 @@ public class MountainEnjoyer {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Menu.showMenu(); // calls the showMenu function in the menu class to.... show the menu
     }
 }
