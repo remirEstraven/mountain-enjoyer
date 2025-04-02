7081fcf67eb3766f2a5989bcc8feb14a52164099
lucas <lucas@peanut-button.lan>
2025-04-02 14:42:56 -0500

    change domain back to model cause it wouldnt let me use it before

--- src/main/java/mountainenjoyer/model/Persistence.java
+++ src/main/java/mountainenjoyer/model/Persistence.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mountainenjoyer.domain;
+package mountainenjoyer.model;
 
 /**
  * Writes users names, high scores, and game progress into a file and retrieves 
--- src/main/java/mountainenjoyer/model/Rules.java
+++ src/main/java/mountainenjoyer/model/Rules.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mountainenjoyer.domain;
+package mountainenjoyer.model;
 
 /**
  * Calculates and manages high score and countdown, and handles 
